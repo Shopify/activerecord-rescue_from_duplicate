db0e414e9f008388ad4e549df5d40c424efbdb80
Guillaume Malette <guillaume@jadedpixel.com>
2013-11-23 17:49:47 -0500

    Add spec for actual AR models

---
+++ spec/support/model.rb
@@ -0,0 +1,90 @@
+require 'active_record'
+require 'json'
+require 'yaml'
+
+AR_VERSION = Gem::Version.new(ActiveRecord::VERSION::STRING)
+AR_4_0 = Gem::Version.new('4.0')
+AR_4_1 = Gem::Version.new('4.1.0.beta')
+
+ActiveRecord::Base.configurations = {
+  'test_sqlite3' => {adapter: 'sqlite3', database: "/tmp/rescue_from_duplicate.db"},
+  'test_postgresql' => {adapter: 'postgresql', database: 'rescue_from_duplicate', username: 'postgres'},
+  'test_mysql' => {adapter: 'mysql2', database: 'rescue_from_duplicate', username: 'root', host: '127.0.0.1', port: 13306},
+}
+
+class CreateAllTables < ActiveRecord::Migration
+  def self.recreate_table(name, *args, &block)
+    execute "drop table if exists #{name}"
+
+    create_table(name, *args) do |t|
+      t.string :name
+      t.integer :size
+    end
+
+    add_index name, :name, :unique => true
+    add_index name, :size, :unique => true
+  end
+
+  def self.up
+    if ENV['MYSQL']
+      ActiveRecord::Base.establish_connection('test_mysql')
+      recreate_table(:mysql_models)
+    end
+
+    if ENV['POSTGRES']
+      ActiveRecord::Base.establish_connection(ENV['POSTGRES_URL'] || 'test_postgresql')
+      recreate_table(:postgresql_models)
+    end
+
+    ActiveRecord::Base.establish_connection('test_sqlite3')
+    recreate_table(:sqlite3_models)
+  end
+end
+
+ActiveRecord::Migration.verbose = false
+CreateAllTables.up
+
+
+module TestModel
+  extend ActiveSupport::Concern
+
+  included do
+    validates_uniqueness_of :name, :rescue_with_errors => true
+    validates_uniqueness_of :size
+  end
+end
+
+if ENV['MYSQL']
+  class MysqlModel < ActiveRecord::Base
+    include TestModel
+
+    establish_connection 'test_mysql'
+  end
+end
+
+if ENV['POSTGRES']
+  class PostgresqlModel < ActiveRecord::Base
+    include TestModel
+
+    establish_connection ENV['POSTGRES_URL'] || 'test_postgresql'
+  end
+end
+
+class Sqlite3Model < ActiveRecord::Base
+  include TestModel
+
+  establish_connection 'test_sqlite3'
+end
+
+Models = [
+  Sqlite3Model
+]
+Models << MysqlModel if defined?(MysqlModel)
+Models << PostgresqlModel if defined?(PostgresqlModel)
+
+
+RSpec.configure do |config|
+  config.before :each do
+    Models.each(&:delete_all)
+  end
+end
---
+++ spec/validator_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+shared_examples 'a model with rescued unique' do
+  describe 'create!' do
+    context 'when catching a race condition' do
+
+      before(:each) {
+        described_class.unscoped.class.any_instance.stub(:exists? => false)
+        described_class.create!(:name => 'toto', :size => 5)
+      }
+
+      it 'raises an ActiveRecord::RecordNotSaved error' do
+        expect{ described_class.create!(:name => 'toto') }.to raise_error(ActiveRecord::RecordNotSaved)
+      end
+
+      it "doesn't save the record" do
+        expect{
+          begin
+            described_class.create!(:name => 'toto')
+          rescue ActiveRecord::RecordNotSaved
+            # NOOP
+          end
+        }.not_to change(described_class, :count)
+      end
+
+      it "rollback the transaction" do
+        expect {
+          begin
+            described_class.transaction do
+              described_class.create(:name => 'not toto', :size => 55)
+              described_class.create!(:name => 'toto')
+            end
+          rescue ActiveRecord::RecordNotSaved
+            # NOOP
+          end
+        }.not_to change(described_class, :count)
+      end
+    end
+
+    context "with no race condition" do
+      it 'saves the model' do
+        expect{ described_class.create!(:name => 'toto') }.to change(described_class, :count).by(1)
+      end
+    end
+  end
+end
+
+describe Sqlite3Model do
+  it_behaves_like 'a model with rescued unique'
+end
+
+if defined?(MysqlModel)
+  describe MysqlModel do
+    it_behaves_like 'a model with rescued unique'
+  end
+end
+
+if defined?(PostgresqlModel)
+  describe PostgresqlModel do
+    it_behaves_like 'a model with rescued unique'
+  end
+end
