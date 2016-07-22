c42752b455176dad5bbd3b156dde85256b67a823
Jean Boussier <jean.boussier@gmail.com>
2016-07-22 15:28:13 +0200

    Handle cases where a model table is missing

--- lib/rescue_from_duplicate/active_record.rb
+++ lib/rescue_from_duplicate/active_record.rb
@@ -15,6 +15,7 @@ module RescueFromDuplicate
 
     klasses.each do |klass|
       klass._rescue_from_duplicate_handlers.each do |handler|
+        next unless klass.connection.table_exists?(klass.table_name)
         unique_indexes = klass.connection.indexes(klass.table_name).select(&:unique)
 
         unless unique_indexes.any? { |index| index.columns.map(&:to_s).sort == handler.columns }
